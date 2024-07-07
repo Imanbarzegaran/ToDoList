e3ab5b962aa2f87f7bc3ff110387802ba31f9605
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-07-07 18:52:34 +0330

    set log out func

--- ToDoList/ToDoList/Views/ProfileView.swift
+++ ToDoList/ToDoList/Views/ProfileView.swift
@@ -15,48 +15,61 @@ struct ProfileView: View {
         NavigationView {
             VStack {
                 // Avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(.blue)
-                    .frame(width: 125, height: 125)
-                    .padding()
-                
-                // info: name , email, member since
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Name:")
-                            .bold()
-                        Text("Iman Barzegaran")
-                    }
-                    .padding()
-                    HStack {
-                        Text("Email:")
-                            .bold()
-                        Text("Iman Barzegaran")
-                    }
-                    .padding()
-                    HStack {
-                        Text("Member since:")
-                            .bold()
-                        Text("Iman Barzegaran")
-                    }
-                    .padding()
-                  
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
                 }
-                .padding()
-                
-                // sign out
-                Button("Log Out") {
-                    viewModel.logOut()
-                }
-                .tint(.red)
-                .padding()
-                Spacer()
                 
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // info: name , email, member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name:")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email:")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Member since:")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+          
+        }
+        .padding()
+        
+        // sign out
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        Spacer()
     }
 }
 
