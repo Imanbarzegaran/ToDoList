aa96c1a087deb0d485fb681495745369ae1d176d
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-07-07 18:29:27 +0330

    create profile tab items
    - move toggle func from profileViewViewModel to ToDoListItemViewViewModel

--- ToDoList/ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by MacBook Air on 7/1/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 
@@ -14,5 +16,20 @@ import Foundation
 class ToDoListItemViewViewModel: ObservableObject {
     
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
     
 }
--- ToDoList/ToDoList/Views/ProfileView.swift
+++ ToDoList/ToDoList/Views/ProfileView.swift
@@ -14,6 +14,45 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
+                // Avatar
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.blue)
+                    .frame(width: 125, height: 125)
+                    .padding()
+                
+                // info: name , email, member since
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Name:")
+                            .bold()
+                        Text("Iman Barzegaran")
+                    }
+                    .padding()
+                    HStack {
+                        Text("Email:")
+                            .bold()
+                        Text("Iman Barzegaran")
+                    }
+                    .padding()
+                    HStack {
+                        Text("Member since:")
+                            .bold()
+                        Text("Iman Barzegaran")
+                    }
+                    .padding()
+                  
+                }
+                .padding()
+                
+                // sign out
+                Button("Log Out") {
+                    viewModel.logOut()
+                }
+                .tint(.red)
+                .padding()
+                Spacer()
                 
             }
             .navigationTitle("Profile")
